7d697362354afa1b5dc50acf7cdefecad0ab8cd1
Ionică Bizău <bizauionica@yahoo.com>
2014-10-08 16:59:15 +0300

    Testing how to create tmux splits

--- start.sh
+++ start.sh
@@ -1,1 +1,14 @@
-tmux
+#!/bin/bash
+tmux new-session -d -s tmuxos
+tmux split-window -d -t 0 -v
+tmux split-window -d -t 0 -h
+tmux split-window -d -t 0 -v
+tmux split-window -d -t 2 -v
+
+tmux send-keys -t 0 'echo "hello world"' enter
+tmux send-keys -t 1 'htop' enter
+
+tmux send-keys -t 2 'apt-get moo' enter
+tmux send-keys -t 3 'echo "hi"' enter
+
+tmux attach
