4221fe3d6bf00125bc58f36354ec2dde1867d381
Ionică Bizău <bizauionica@yahoo.com>
2014-10-08 19:54:58 +0300

    Absolute path to splash.js

--- start.sh
+++ start.sh
@@ -2,6 +2,6 @@
 tmux new-session -d -s tmuxos
 tmux split-window -v -t 0 -p 20
 
-tmux send-keys -t 0 'node splash.js' enter
+tmux send-keys -t 0 "node $(dirname -- $0)/splash.js" enter
 
 tmux attach
