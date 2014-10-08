4b6226f982bde327a7fd041551c696da355675df
Ionică Bizău <bizauionica@yahoo.com>
2014-10-08 17:11:40 +0300

    Create splash

--- splash.js
+++ splash.js
@@ -1,1 +1,14 @@
 const DESCRIPTION = "The awesome power of command line is finally releaved.";
+
+var ImageToAscii = require("image-to-ascii")
+  , myImage = new ImageToAscii ({
+        resize: {
+            width:  "100%"
+        }
+      , colored: true
+    })
+  ;
+
+myImage.convert("../resources/splash/tmuxos.png", function(err, converted) {
+    console.log(err || converted);
+});
