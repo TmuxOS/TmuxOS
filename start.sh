db52f280bd7bbd45725e9bc91cb8f8cbdcdc66d5
Ionică Bizău <bizauionica@yahoo.com>
2014-10-08 18:08:10 +0300

    Show splash screen

--- start.sh
+++ start.sh
@@ -1,14 +1,7 @@
 #!/bin/bash
 tmux new-session -d -s tmuxos
-tmux split-window -d -t 0 -v
-tmux split-window -d -t 0 -h
-tmux split-window -d -t 0 -v
-tmux split-window -d -t 2 -v
+tmux split-window -v -t 0 -p 20
 
-tmux send-keys -t 0 'echo "hello world"' enter
-tmux send-keys -t 1 'htop' enter
-
-tmux send-keys -t 2 'apt-get moo' enter
-tmux send-keys -t 3 'echo "hi"' enter
+tmux send-keys -t 0 'node splash.js' enter
 
 tmux attach
