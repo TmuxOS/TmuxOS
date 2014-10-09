b76b42149f2a8acdb06756f457c82407527b2f8b
Ionică Bizău <bizauionica@yahoo.com>
2014-10-09 22:11:11 +0300

    Load tmux correclty

--- start.sh
+++ start.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
-tmux new-session -d -s tmuxos
+export TERM=xterm-256color
+
+tmux -2 -f /home/$USER/.tmux.conf new-session -d -s tmuxos
+tmux source-file /home/$USER/.tmux.conf
 tmux split-window -v -t 0 -p 20
 
 tmux send-keys -t 0 "node $(dirname -- $0)/splash.js" enter
